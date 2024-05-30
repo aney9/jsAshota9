53e6c1e5cf501fab3071e9076d7c80cb7de6ea96
aney9 <isip_t.a.murashov@mpt.ru>
2024-05-31 01:46:40 +0300

    uuu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 let massiv = [];
 
+function add(){
+    const surname = document.getElementById("Surname");
+    const name = document.getElementById("Name");
+    const middlename = document.getElementById("Middlename");
+    const fanid = document.getElementById("Fanid");
+    const phone = document.getElementById("Phone");
+    const sektor = document.getElementById("Sektor");
+    const ryad = document.getElementById("Ryad");
+    const mesto = document.getElementById("Mesto");
 
+    const Surname = surname.value.trim();
+    const Name = name.value.trim();
+    const Middlename = middlename.value.trim();
+    const Fanid = fanid.value.trim();
+    const Sektor = sektor.value.trim();
+    const Ryad = ryad.value.trim();
+    const Mesto = mesto.value.trim();
+
+    if(!Surname || !Name || !Middlename || )
+
+
+
+}
 
