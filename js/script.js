061964522b76f5631e5996beefa5229a3c36d0b3
aney9 <isip_t.a.murashov@mpt.ru>
2024-06-01 01:58:53 +0300

    snova

--- js/script.js
+++ js/script.js
@@ -13,14 +13,45 @@ function add(){
     const Surname = surname.value.trim();
     const Name = name.value.trim();
     const Middlename = middlename.value.trim();
-    const Fanid = fanid.value.trim();
-    const Sektor = sektor.value.trim();
-    const Ryad = ryad.value.trim();
-    const Mesto = mesto.value.trim();
+    const Fanid = parseInt(fanid.value);
+    const Phone = phone.value.trim();
+    const Sektor = parseInt(sektor.value);
+    const Ryad = parseInt(ryad.value);
+    const Mesto = parseInt(mesto.value);
 
-    if(!Surname || !Name || !Middlename || )
+    if(!Surname || !Name || !Middlename || fanid <= 0 || !Phone || Sektor <= 0 || Ryad <= 0 || Mesto <= 0){
+        alert('Ошибка ввода');
+        return;
+    }
+
+    const tickets{
+        Surname: Surname,
+        Name: Name,
+        Middlename: Middlename,
+        Fanid: Fanid,
+        Phone: Phone,
+        Sektor: Sektor,
+        Ryad: Ryad,
+        Mesto: Mesto,
+        displayInfo: function (){
+            return 'Имя: $(this.Name) фамилия: $(this.Surname) отчество: $(this.Middlename) FanId: $(this.Fanid) Телефон: $(this.Phone) Сектор: $(this.Sektor) Ряд: $(this.Ryad) Место: $(this.Mesto)'
+        }
+    };
+
+    massiv.push(tickets);
 
 
+function update(){
+    const list = document.getElementById('list');
+    list.innerHTML = '';
+    massiv.forEach(tickets, index) => {
+        const li = document.createElement('li');
+        li.classList.add('')
+
+
+    }
+
+}
 
 }
 
